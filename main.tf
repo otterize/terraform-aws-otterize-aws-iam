d4626ad3de43fd7c6bf732bc5f8a3b24a40fe39c
Ori Shoshan <ori@otterize.com>
2024-03-17 10:43:34 +0100

    Add awscc provider

--- main.tf
+++ main.tf
@@ -4,11 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    awscc = {
+      source = "hashicorp/awscc"
+      version = "0.71.0"
+    }
   }
 
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
+  region = var.aws_region
+}
+
+provider "awscc" {
   region = var.aws_region
 }
